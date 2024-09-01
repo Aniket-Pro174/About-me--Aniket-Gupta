6294f534799fb7edff994e6b3423802ea98bc08a
Aniket-Pro174 <125129589+Aniket-Pro174@users.noreply.github.com>
2024-09-01 14:34:17 +0530

    Files Added

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Personal Information Page Loaded Successfully!");
+    // Additional JavaScript functionalities can be added here.
+});
